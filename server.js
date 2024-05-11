0d013723e41f46521a61d88f02f485feaa19f798
Cheikhouna Fall <cheikhounafall20012@gmail.com>
2024-05-11 19:31:47 +0000

    Premier commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    console.log('A new client Connected!');
+    ws.send('Welcome New Client!');
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+
+        // Vous pouvez ici décider de renvoyer le message à tous les clients connectés
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', function () {
+        console.log('Client has disconnected!');
+    });
+});
+
+console.log(`WebSocket server started on ws://localhost:8080`);
